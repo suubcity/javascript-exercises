29d0091766d6df22ef7b667e92fc5ac124f5f407
suubcity <suubcity@outlook.com>
2020-07-16 13:59:40 -0700

    Finsihed removeFromArray task

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (let i = 0; i < args.length; i++) {       
+        array = array.filter(value => value !==  args[i]);
+    }
+    return array;
 }
 
 module.exports = removeFromArray
