3fc2e7ac8d2a980412129a9a13da9a1b61ee1c68
suubcity <suubcity@outlook.com>
2020-07-17 11:28:55 -0700

    finsihed leapyear task

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  if (year % 400 === 0) {
+    return true;
+  }
+  if (year % 100 !== 0 && year % 4 === 0) {
+    return true;
+  }
+  return false;
+};
 
-}
+module.exports = leapYears;
 
-module.exports = leapYears
+// Leap years are years divisible by four (like 1984 and 2004). However, years divisible by 100 are not leap years (such as 1800 and 1900) unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years). (Yes, it's all pretty confusing, but not as confusing as having July in the middle of the winter, which is what would eventually happen.)
